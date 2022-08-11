12f23ea605692e5f962f1827c7366e4bb6573c91
Vladimir Kunats <vladimir.kunats@gmail.com>
2022-08-11 21:37:35 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if(str.length%2!==0)
+      return false;
+  newStr = str.split('');
+  stack = [];
+  for(let i = 0; i < newStr.length; i++) {
+      for (let j = 0; j < bracketsConfig.length; j++) {
+          if(newStr[i]===bracketsConfig[j][0])
+          stack.push(bracketsConfig[j][0]);
+          if(bracketsConfig[j][0]!==bracketsConfig[j][1]){
+              if(newStr[i]===bracketsConfig[j][1]){
+                  if(bracketsConfig[j][0]!==stack.pop())
+                      return false;
+              }
+          }
+          else{
+              if(stack[stack.length - 1] === bracketsConfig[j][0])
+                  stack.pop();
+          }
+      }
+  }
+
+  return stack.length === 0;
 }
